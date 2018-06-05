47a040cdf9f2d40530f0e5549d7fc45f8360d0d7
Graeme Watt <Graeme.Watt@durham.ac.uk>
2018-06-05 18:55:53 +0100

    check.py: add checks for additional_resources and length of 'values'
    * Check for presence of local files given as additional_resources.
    * Check that the length of the 'values' list is consistent for each of the independent_variables and dependent_variables.
    * Same checks have now been implemented in submission.py of the main web app (HEPData/hepdata).
    
    Signed-off-by: Graeme Watt <graeme.watt@durham.ac.uk>

--- scripts/check.py
+++ scripts/check.py
@@ -8,6 +8,8 @@ Or specify the directory as an additional argument:
   ./check.py directory
 """
 
+import os.path
+
 # Get directory as optional command-line argument.
 import sys
 if len(sys.argv) == 2:
@@ -39,7 +41,7 @@ except ImportError:
     validator_imported = False
 
 # Give location of the submission.yaml file.
-submission_file_path = directory + '/submission.yaml' if directory else 'submission.yaml'
+submission_file_path = os.path.join(directory, 'submission.yaml')
 
 # Validate the submission.yaml file if validator imported.
 if validator_imported:
@@ -59,23 +61,44 @@ with open(submission_file_path, 'r') as stream:
     # Loop over all YAML documents in the submission.yaml file.
     for doc in docs:
 
+        # Skip empty YAML documents.
+        if not doc:
+            continue
+
+        # Check for presence of local files given as additional_resources.
+        if 'additional_resources' in doc:
+            for resource in doc['additional_resources']:
+                location = os.path.join(directory, resource['location'])
+                if not location.startswith('http'):
+                    if not os.path.isfile(location):
+                        print('%s is missing.' % location)
+
         # Check for non-empty YAML documents with a 'data_file' key.
-        if doc and 'data_file' in doc:
+        if 'data_file' in doc:
 
             # Extract data file from YAML document.
             data_file_path = directory + '/' + doc['data_file'] if directory else doc['data_file']
 
+            # Just try to load YAML data file without validating schema.
+            # Script will terminate with an exception if there is a problem.
+            contents = yaml.safe_load(open(data_file_path, 'r'))
+
             # Validate the YAML data file if validator imported.
-            if validator_imported:
+            if not validator_imported:
+                print('%s is valid YAML.' % data_file_path)
+            else:
                 data_file_validator = DataFileValidator()
-                is_valid_data_file = data_file_validator.validate(file_path=data_file_path)
+                is_valid_data_file = data_file_validator.validate(file_path=data_file_path, data=contents)
                 if not is_valid_data_file:
                     print('%s is invalid HEPData YAML.' % data_file_path)
                     data_file_validator.print_errors(data_file_path)
                 else:
-                    print('%s is valid HEPData YAML.' % data_file_path)
-            else:
-                # Just try to load YAML data file without validating schema.
-                # Script will terminate with an exception if there is a problem.
-                contents = yaml.safe_load(open(data_file_path, 'r'))
-                print('%s is valid YAML.' % data_file_path)
+                    # Check that the length of the 'values' list is consistent for
+                    # each of the independent_variables and dependent_variables.
+                    indep_count = [len(indep['values']) for indep in contents['independent_variables']]
+                    dep_count = [len(dep['values']) for dep in contents['dependent_variables']]
+                    if len(set(indep_count + dep_count)) > 1: # if more than one unique count
+                        print("%s has inconsistent length of 'values' list: " % data_file_path +
+                              "independent_variables%s, dependent_variables%s." % (str(indep_count), str(dep_count)))
+                    else:
+                        print('%s is valid HEPData YAML.' % data_file_path)
